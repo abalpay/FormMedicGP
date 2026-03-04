c85f82ecee6cdd750235ad05e2644005627f366b
abalpay <65846020+abalpay@users.noreply.github.com>
2026-03-04 13:02:12 +1100

    fix: update seed email in database seeding script

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -3,7 +3,7 @@
 
 do $$
 declare
-  seed_email constant text := 'test@formbridgegp.dev';
+  seed_email constant text := 'test@test.com';
   seed_password constant text := 'testtest';
   seed_user_id uuid;
 begin
